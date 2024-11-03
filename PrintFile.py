e3ba62601948f3f1d088cb684ecf8ffa046e60f1
sdroth <samantharoth@unomaha.edu>
2024-11-03 20:01:28 +0000

    PrintFile_Final

--- PrintFile.py
+++ PrintFile.py
@@ -2,10 +2,12 @@ def main():
   myFile = open("qbdata.txt", 'r')
 
   for line in myFile:
-    print (line)
-
+    info = line.split("	")
+    td = info[7]
+    name = info[0]
+    rate = info[9]
+    print(name, "had a rating of", rate, "and threw", td, "touchdowns.")
   myFile.close()
-
 
 if __name__ == '__main__':
   main()
