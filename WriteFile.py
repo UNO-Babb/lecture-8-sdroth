7b42f44dcd868c377f22d3d390607e6e26f7ce17
sdroth <samantharoth@unomaha.edu>
2024-11-03 20:13:42 +0000

    WriteFile_final

--- WriteFile.py
+++ WriteFile.py
@@ -4,12 +4,14 @@ def main():
 
   outFile = open("dice_rolls.txt", 'w')
 
-  die1 = random.randint(1,6)
-  die2 = random.randint(1,6)
-  total = die1 + die2
+  outFile.write("Dice1 Dice2 Total\n")
+  for i in range(10):
+    die1 = random.randint(1,6)
+    die2 = random.randint(1,6)
+    total = die1 + die2
 
-  output = str(die1) + " " + str(die2) + " " + str(total)
-  outFile.write(output)
+    output = str(die1) + " " + str(die2) + " " + str(total) + "\n"
+    outFile.write(output)
 
   outFile.close()
 
